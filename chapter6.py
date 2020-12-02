1295b017a8661bbb5d4c390593545fa28679a2bd
J-TechTips <jtecktips@gmail.com>
2020-12-01 17:38:04 -0800

    structured-english

---
+++ chapter6.py
@@ -0,0 +1,13 @@
+"""
+To start, we will generate a random integer between 1 and 20, and 
+based on the restult of the random number, we check to see if it falls under certain range
+if the number is greater than 15, then the results will be "Cherries"
+otherwise if the number is > 10, then the result will be "Orange"
+otherwise if the number is > 5, then the result will be "Plum"
+otherwise if the number is > 2, then the result will be "Melon"
+otherwise if the number is > 1, then the result will be "Bell"
+if the number is not any of the above, then the result will be "Bar"
+
+we iterate over using a loop three trimes and print the results to the user.
+
+"""
