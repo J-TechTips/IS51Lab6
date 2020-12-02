52b0199a79da948c44e9180e1a55c6f738e6fbb9
J-TechTips <jtecktips@gmail.com>
2020-12-01 17:40:48 -0800

    psuedo-code

--- chapter6.py
+++ chapter6.py
@@ -11,3 +11,25 @@ if the number is not any of the above, then the result will be "Bar"
 we iterate over using a loop three trimes and print the results to the user.
 
 """
+
+"""
+import  random
+num = generate radom number
+
+if num is greater than 15, 
+    then the results will be "cherries"
+otherwise if num is > 10, 
+    then the result will be "Orange"
+otherwise if the number is > 5, 
+    then the result will be "Plum"
+otherwise if the number is > 2, 
+    then the result will be "Melon"
+otherwise if the number is > 1, 
+    then the result will be "Bell"
+otherwise 
+    the result will be "bar"
+
+loop three times
+    print the output (fruit) to the user
+
+"""
